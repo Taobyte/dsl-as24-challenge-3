4d22adc1d770238c7c62149a8e8f8027e2cb82c2
sphyrna15 <timmysnakes@gmail.com>
2024-10-21 17:21:47 +0200

    add new classes for moving STFT operation into model

--- src/data.py
+++ src/data.py
@@ -1,12 +1,15 @@
-import torch as th
 import os
 import glob
-import numpy as np
+import random
+
+import keras
 import scipy
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader, Subset
+import torch as th
+import numpy as np
 from numpy import ndarray
 from argparse import Namespace
+from torch.utils.data import Dataset, DataLoader, Subset
 
 
 def get_dataloaders(args: Namespace) -> tuple[DataLoader, DataLoader]:
@@ -589,3 +592,157 @@ class DeepDenoiserDatasetTest(Dataset):
             th_noisy = th.from_numpy(noisy)
 
             return th_noisy, th_mask, eq, noise
+
+
+def get_signal_noise_assoc(signal_path, noise_path, train=True):
+    """generates a singal to noise file association from folders
+    Allows defining a standard data association for reproducibility. 
+    Args:
+        - signal_path: path to the signal folder
+        - noise_path: path to the noise_folder
+        - train: boolean (use training set)
+    Returns:
+        - a list of tuples, where each tuple contains:
+          (signal file, noise file, random SNR, random event shift)
+
+    """
+
+    if train:
+        signal_files = glob.glob(f"{signal_path}/train/**/*.npz", recursive=True)
+        noise_files = glob.glob(f"{noise_path}/train/**/*.npz", recursive=True)
+
+    else:
+        signal_files = glob.glob(f"{signal_path}/validation/**/*.npz", recursive=True)
+        noise_files = glob.glob(f"{noise_path}/validation/**/*.npz", recursive=True)
+
+    # shuffle
+    random.shuffle(signal_files)
+    random.shuffle(noise_files)
+
+    assoc = []
+    for i in range(len(signal_files)):
+        n = np.random.randint(0, len(noise_files))
+        snr_random = np.random.uniform(0.2,1.5)
+        event_shift = np.random.randint(1000,6000)
+        assoc.append((signal_files[i], noise_files[n], snr_random, event_shift))
+
+    return assoc
+
+
+class InputSignals(th.utils.data.Dataset):
+
+    def __init__(self, signal_noise_association:list):
+        """
+        Args:
+            signal_noise_association: a list containing tuples for signal and noise filenames
+        """
+
+        self.signal_noise_assoc = signal_noise_association
+        self.signal_length = 6120
+
+    def __len__(self) -> int:
+        return len(self.signal_noise_assoc)
+
+    def __getitem__(self, idx) -> th.Tensor:
+
+        while True:
+
+            eq = np.load(self.signal_noise_assoc[idx][0], allow_pickle=True)
+            noise = np.load(self.signal_noise_assoc[idx][1], allow_pickle=True)
+            snr_random = self.signal_noise_assoc[idx][2]
+            event_shift = self.signal_noise_assoc[idx][3]
+            
+            Z_eq = eq["earthquake_waveform_Z"][event_shift : event_shift + self.signal_length]
+            N_eq = eq["earthquake_waveform_N"][event_shift : event_shift + self.signal_length]
+            E_eq = eq["earthquake_waveform_E"][event_shift : event_shift + self.signal_length]
+            eq_stacked = np.stack([Z_eq, N_eq, E_eq], axis=0)
+
+            Z_noise = noise["noise_waveform_Z"][:self.signal_length]
+            N_noise = noise["noise_waveform_N"][:self.signal_length]
+            E_noise = noise["noise_waveform_E"][:self.signal_length]
+
+            if (
+                Z_noise.shape != N_noise.shape
+                or Z_noise.shape != E_noise.shape
+                or N_noise.shape != E_noise.shape
+            ):
+                idx=idx+1
+                continue
+
+            noise_stacked = np.stack([Z_noise, N_noise, E_noise], axis=0)
+
+            signal_std = np.std(eq_stacked[:,6000-event_shift:6500-event_shift], axis=1).reshape(-1,1)  
+            noise_std = np.std(noise_stacked[:,6000-event_shift:6500-event_shift], axis=1).reshape(-1,1)
+            snr_original = signal_std / noise_std
+            # change the SNR
+            noise_stacked = noise_stacked * snr_original  # rescale noise so that SNR=1
+            eq_stacked = eq_stacked * snr_random  # rescale event to desired SNR
+            noisy_eq = eq_stacked + noise_stacked # recombine
+
+            return noisy_eq
+        
+
+class EventMasks(th.utils.data.Dataset):
+
+    def __init__(self, signal_noise_association:list):
+        """
+        Args:
+            signal_noise_association: a list containing tuples for signal and noise filenames
+        """
+
+        self.signal_noise_assoc = signal_noise_association
+        self.signal_length = 6120
+
+        # scipy hyperparameters
+        self.frame_length = 100
+        self.frame_step = 24
+        self.fft_size = 126
+
+    def __len__(self) -> int:
+        return len(self.signal_noise_assoc)
+
+    def __getitem__(self, idx) -> th.Tensor:
+
+        while True:
+
+            eq = np.load(self.signal_noise_assoc[idx][0], allow_pickle=True)
+            noise = np.load(self.signal_noise_assoc[idx][1], allow_pickle=True)
+            snr_random = self.signal_noise_assoc[idx][2]
+            event_shift = self.signal_noise_assoc[idx][3]
+            
+            Z_eq = eq["earthquake_waveform_Z"][event_shift : event_shift + self.signal_length]
+            N_eq = eq["earthquake_waveform_N"][event_shift : event_shift + self.signal_length]
+            E_eq = eq["earthquake_waveform_E"][event_shift : event_shift + self.signal_length]
+            eq_stacked = np.stack([Z_eq, N_eq, E_eq], axis=0)
+
+            Z_noise = noise["noise_waveform_Z"][:self.signal_length]
+            N_noise = noise["noise_waveform_N"][:self.signal_length]
+            E_noise = noise["noise_waveform_E"][:self.signal_length]
+
+            if (
+                Z_noise.shape != N_noise.shape
+                or Z_noise.shape != E_noise.shape
+                or N_noise.shape != E_noise.shape
+            ):
+                idx=idx+1
+                continue
+
+            noise_stacked = np.stack([Z_noise, N_noise, E_noise], axis=0)
+
+            signal_std = np.std(eq_stacked[:,6000-event_shift:6500-event_shift], axis=1).reshape(-1,1)  
+            noise_std = np.std(noise_stacked[:,6000-event_shift:6500-event_shift], axis=1).reshape(-1,1)
+            snr_original = signal_std / noise_std
+            # change the SNR
+            noise_stacked = noise_stacked * snr_original  # rescale noise so that SNR=1
+            eq_stacked = eq_stacked * snr_random  # rescale event to desired SNR
+
+            stft = keras.ops.stft(eq_stacked, self.frame_length, self.frame_step, self.fft_size)
+            stft_eq = np.concatenate([stft[0],stft[1]], axis=0)
+
+            stft = keras.ops.stft(noise_stacked, self.frame_length, self.frame_step, self.fft_size)
+            stft_noise = np.concatenate([stft[0],stft[1]], axis=0)
+
+            mask = np.abs(stft_eq) / (np.abs(stft_noise) + np.abs(stft_eq) + 1e-4)
+            
+
+            return mask
