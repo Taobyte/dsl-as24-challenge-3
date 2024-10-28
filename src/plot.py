5e0ac0fa43de9e6d2f2e460329c1e92a84c73b1e
sphyrna15 <timmysnakes@gmail.com>
2024-10-28 11:43:22 +0100

    fix errorbar linewidth mismatch

--- src/plot.py
+++ src/plot.py
@@ -14,8 +14,8 @@ def compare_two(df1:pd.DataFrame, df2:pd.DataFrame, label1:str, label2:str):
                 color='cornflowerblue', ecolor='lightsteelblue', elinewidth=2, capthick=2, markersize=8)
 
     # Plot the second model with error bars
-    ax.errorbar(df2['snr'], df2['cc_mean'], yerr=df2['cc_std'], fmt='s-', capsize=6, label=label2, 
-                color='olivedrab', ecolor='yellowgreen', elinewidth=3, capthick=3, markersize=8)
+    ax.errorbar(df2['snr'], df2['cc_mean'], yerr=df2['cc_std'], fmt='s-', capsize=5, label=label2, 
+                color='olivedrab', ecolor='yellowgreen', elinewidth=2, capthick=2, markersize=8)
 
     # Set the title and labels
     ax.set_title('Comparison of Denoising Models', fontsize=16)
