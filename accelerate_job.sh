b5f537c31b2e5056264ac4a709009634589b9989
Clemens Keusch <ckeusch@eu-login-28.euler.ethz.ch>
2024-12-10 23:28:21 +0100

    wip

--- accelerate_job.sh
+++ accelerate_job.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem-per-cpu=40g
 #SBATCH --gpus=4
 #SBATCH --gres=gpumem:24g
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=accelerate_logs/job%j.out
 #SBATCH --error=accelerate_logs/job_%j.err
-# Activate your conda environment (if using)
+
 source /cluster/home/ckeusch/miniconda3/bin/activate
 conda activate CDiffSD
-# Run the command add :
+
 accelerate launch --multi-gpu --mixed_precision=fp16 src/main.py 
