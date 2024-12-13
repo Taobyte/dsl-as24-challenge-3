859b0138170ec5eb900a79d261e9c9f5c3ec20f9
Clemens Keusch <ckeusch@student-cluster.inf.ethz.ch>
2024-12-13 11:44:03 +0100

    added assoc to dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,10 +1,12 @@
 import glob
 import logging
+import random
 
 import omegaconf
 import torch as th
 from torch.utils.data import Dataset, DataLoader, Subset
 import numpy as np
+import einops
 
 from src.utils import Mode, Model
 from src.stft import get_mask
@@ -94,6 +96,179 @@ class EQDataset(Dataset):
         if self.mode == Mode.TEST:
             return (self.file_eq[index], self.file_noise[index], self.assoc[index][3])
         return (self.file_eq[index], self.file_noise[index])
+
+
+def get_signal_noise_assoc(
+    signal_path: str,
+    noise_path: str,
+    mode: Mode,
+    size_testset=1000,
+    snr=lambda: np.random.uniform(0.1, 1.1),
+) -> list[tuple[str, str, float, int]]:
+    """generates a signal to noise file association from folders
+    Allows defining a standard data association for reproducibility.
+    Args:
+        - signal_path: path to the signal folder
+        - noise_path: path to the noise_folder
+        - train: boolean (use training set)
+        - mode: enum TRAIN, VALIDATION, TEST
+        - size_testset: integer for fixing testset size
+        - snr: float for fixing the signal to noise ratio
+    Returns:
+        - a list of tuples, where each tuple contains:
+          (signal file, noise file, random SNR, random event shift)
+
+    """
+    print(mode)
+
+    if mode == Mode.TRAIN:
+        signal_files = glob.glob(f"{signal_path}/train/**/*.npz", recursive=True)
+        noise_files = glob.glob(f"{noise_path}/train/**/*.npz", recursive=True)
+
+    elif mode == Mode.VALIDATION:
+        signal_files = glob.glob(
+            f"{signal_path}/validation/**/*.npz", recursive=True
+        )  # [:-size_testset]
+        noise_files = glob.glob(
+            f"{noise_path}/validation/**/*.npz", recursive=True
+        )  # [:-size_testset]
+
+    elif mode == Mode.TEST:
+        signal_files = glob.glob(f"{signal_path}/**/*.npz", recursive=True)
+        noise_files = glob.glob(f"{noise_path}/**/*.npz", recursive=True)
+
+    else:
+        assert False, f"Mode {mode} not supported!"
+
+    # shuffle
+    signal_files = signal_files + signal_files
+    noise_files = noise_files + noise_files
+    random.shuffle(signal_files)
+    random.shuffle(noise_files)
+
+    assoc = []
+    for i in range(len(signal_files)):
+        n = np.random.randint(0, len(noise_files))
+        snr_random = snr()
+        r = np.random.randint(0, 5)
+        if r == 0 and not (mode == Mode.TEST):
+            event_shift = np.random.randint(0, 1000)
+        else:
+            event_shift = np.random.randint(2500, 6000)
+        assoc.append((signal_files[i], noise_files[n], snr_random, event_shift))
+
+    return assoc
+
+
+def compute_train_dataset(signal_length, mode, memmap):
+    num = 7
+    if mode == Mode.TEST:
+        num = 1
+        signal_path = (
+            "/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/test/event"
+        )
+        noise_path = (
+            "/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/test/noise"
+        )
+    else:
+        signal_path = "/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/event"
+        noise_path = "/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/noise"
+    if mode == Mode.TRAIN:
+        dataset_eq_name = f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/train_eq_00{num}"
+        dataset_noise_name = f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/train_noise_00{num}"
+    elif mode == Mode.TEST:
+        dataset_eq_name = (
+            f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/tst_eq_00{num}"
+        )
+        dataset_noise_name = f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/tst_noise_00{num}"
+    else:
+        dataset_eq_name = (
+            f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/val_eq_00{num}"
+        )
+        dataset_noise_name = f"/home/tim/Documents/Data-Science_MSc/DSLab/earthquake_data/val_noise_00{num}"
+    if not mode == Mode.TEST:
+        assoc = get_signal_noise_assoc(
+            signal_path,
+            noise_path,
+            mode,
+            size_testset=1000,
+            snr=lambda: np.random.uniform(0.1, 1.8),
+        )
+    else:
+        assoc = get_signal_noise_assoc(
+            signal_path, noise_path, mode, size_testset=1000, snr=lambda: 1.0
+        )
+    full = []
+    earthquakes = []
+    noises = []
+
+    for eq_file, noise_file, snr_random, event_shift in assoc:
+        eq = np.load(eq_file, allow_pickle=True)
+        noise = np.load(noise_file, allow_pickle=True)
+
+        Z_eq = eq["earthquake_waveform_Z"][event_shift : event_shift + signal_length]
+        N_eq = eq["earthquake_waveform_N"][event_shift : event_shift + signal_length]
+        E_eq = eq["earthquake_waveform_E"][event_shift : event_shift + signal_length]
+        eq_stacked = np.stack([Z_eq, N_eq, E_eq], axis=1)
+
+        Z_noise = noise["noise_waveform_Z"][:signal_length]
+        N_noise = noise["noise_waveform_N"][:signal_length]
+        E_noise = noise["noise_waveform_E"][:signal_length]
+        noise_stacked = np.stack([Z_noise, N_noise, E_noise], axis=1)
+        if len(noise_stacked) == 0:
+            print("noise", noise_file)
+
+        max_val = np.max(np.abs(noise_stacked)) + 1e-10
+        noise_stacked = noise_stacked / max_val
+        if event_shift < 6000 - signal_length:
+            earthquakes.append(eq_stacked)
+            noises.append(noise_stacked)
+            continue
+        max_val = np.max(np.abs(eq_stacked)) + 1e-10
+        eq_stacked = eq_stacked / max_val
+
+        signal_std = np.std(
+            eq_stacked[6000 - event_shift : 6500 - event_shift, :], axis=0
+        )
+        noise_std = np.std(
+            noise_stacked[6000 - event_shift : 6500 - event_shift, :], axis=0
+        )
+        snr_original = signal_std / (noise_std + 1e-10)
+
+        # change the SNR
+        noise_stacked = noise_stacked * snr_original  # rescale noise so that SNR=1
+        eq_stacked = eq_stacked * snr_random  # rescale event to desired SNR
+        if np.isnan(noise_stacked).any():
+            print("std", snr_original)
+            print("shift", event_shift)
+            print("len", len(Z_noise))
+            print(Z_noise[6000 - event_shift : 6500 - event_shift])
+
+        output = np.stack([eq_stacked.T, noise_stacked.T], axis=0)
+        output = einops.rearrange(output, "n d t -> (n d) t")
+
+        earthquakes.append(eq_stacked)
+        noises.append(noise_stacked)
+        full.append(output)
+
+    # return np.array(full)
+
+    full = np.array(full)
+    print(full.shape)
+    earthquakes = einops.rearrange(np.array(earthquakes), "n t d -> n d t")
+    noises = einops.rearrange(np.array(noises), "n t d -> n d t")
+    print(f"Shapes of eq: {earthquakes.shape}, and noise: {noises.shape}")
+    if memmap:
+        pass
+        # file = np.memmap(dataset_name, dtype=np.float32, mode="w+", shape=full.shape)
+        # file[:] = full
+        # file.flush
+    else:
+        np.save(dataset_eq_name, earthquakes, allow_pickle=True)
+        np.save(dataset_noise_name, noises, allow_pickle=True)
+        # np.save(dataset_name[:-4], full, allow_pickle=True)
+    if mode == Mode.TEST:
+        np.save(dataset_eq_name[:-4] + "_assoc", assoc, allow_pickle=True)
 
 
 class EQRandomDataset(Dataset):
